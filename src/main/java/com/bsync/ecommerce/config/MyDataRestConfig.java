a424168dd09e62549c8901a30ba4d6a1d49603fe
tharhtookyaw79 <thktharhtookyaw7995@gmail.com>
2023-03-02 17:47:04 +0700

    updates in configuration

---
+++ src/main/java/com/bsync/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,7 @@
+package com.bsync.ecommerce.config;
+
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+}
